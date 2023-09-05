b7b52ae58c8b0b3833eb6825b134b69d5dca97de
manbodhvikram <manbodhvikram@gmail.com>
2023-09-05 14:16:53 +0530

    submitting code-Tue Sep 05 2023 14:16:49 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,23 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useEffect, useState} from "react";
 
+import './../styles/App.css';
+
+
+const App=()=>{
+    const[text,setText]=useState("");
+
+   useEffect(()=>{
+
+    console.log(text);
+   },[])
+
+    return (
+        <div className="markdown">
+
+             <input type="text" onChange={(e)=>setText(e.target.value)}></input>
+            <div><h1>{text}</h1></div>
+        </div>
+    )
+}
+export default App;
